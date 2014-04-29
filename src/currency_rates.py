9ecf5c538a7f68e1436e886b9a1a0f5d68d76f68
Alex Bepple <alex@bepple.de>
2014-04-29 19:45:06 +0200

    conform to PEP8

--- src/currency_rates.py
+++ src/currency_rates.py
@@ -1,4 +1,5 @@
-import urllib2, re
+import urllib2
+import re
 from datetime import datetime
 
 _cached_symbols = None
@@ -14,11 +15,12 @@ def currency_symbols():
 
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    url = "http://www.xe.com/iso4217.php" 
+    url = "http://www.xe.com/iso4217.php"
     page = opener.open(url).read()
 
     symbols = {}
-    currency_regex = r'href="/currency/[^>]+>(...)</a></td><td class="[^"]+">([A-Za-z ]+)'
+    currency_regex = \
+        r'href="/currency/[^>]+>(...)</a></td><td class="[^"]+">([A-Za-z ]+)'
     for m in re.finditer(currency_regex, page):
         symbols[m.group(1)] = m.group(2)
 
@@ -35,14 +37,14 @@ def conversion_rate(from_currency, to_currency):
         raise ValueError("Invalid to currency: {}".format(to_currency))
 
     url = ("http://www.gocurrency.com/v2/dorate.php?inV=1&from={}&" +
-            "to={}&Calculate=Convert").format(from_currency, to_currency)
+           "to={}&Calculate=Convert").format(from_currency, to_currency)
     page = urllib2.urlopen(url).read()
 
     start = page.rindex('<div id="converter_results"><ul><li>')
     substring = page[start:]
-    start_of_interesting_stuff = substring.index('<strong>') + 3
-    end_of_interesting_stuff = substring.index('</strong>', start_of_interesting_stuff)
-    interesting_stuff = substring[start_of_interesting_stuff:end_of_interesting_stuff]
+    start_of_payload = substring.index('<strong>') + 3
+    end_of_payload = substring.index('</strong>', start_of_payload)
+    interesting_stuff = substring[start_of_payload:end_of_payload]
     parts = interesting_stuff.split('=')
     value = parts[1].strip().split(' ')[0]
     return float(value)
